490a01ee09a4cc43656fca1a8be6182f8164a57c
EvgeniaOv <gralph@rambler.ru>
2022-01-12 20:50:07 +0300

    LambdaVersion

---
+++ src/test/java/test/LambdaVersion.java
@@ -0,0 +1,41 @@
+package test;
+
+import com.codeborne.selenide.Condition;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selectors.withText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+import static io.qameta.allure.Allure.step;
+import static javax.swing.text.html.parser.DTDConstants.NUMBER;
+import static org.openqa.selenium.By.linkText;
+import static org.openqa.selenium.By.partialLinkText;
+
+public class LambdaVersion {
+    private static final String REPOSITORY = "eroshenkoam/allure-example";
+    private static final int NUMBER = 68;
+
+    @Test
+    public void testIssueSearchLambda() {
+
+        step("Открываем гласную страницу", () -> {
+            open("https://github.com");
+        });
+        step("Поиск репозитория" + REPOSITORY, () -> {
+            $(".header-search-input").click();
+            $(".header-search-input").sendKeys(REPOSITORY);
+            $(".header-search-input").submit();
+        });
+        step("Переходим в репозиторий" + REPOSITORY, () -> {
+            $(linkText(REPOSITORY)).click();
+        });
+        step("Открываем таб Issures", () -> {
+            $(partialLinkText("Issues")).click();
+        });
+        step("Проверяем наличие Issures с номером" + NUMBER, () -> {
+            $(withText("#" + NUMBER)).should(Condition.visible);
+        });
+
+    }
+}
+
